17d845fe629c5425d9145560cf27725739b232b4
Prahadeeshwaran <95561624+prahadeeshwaran-k@users.noreply.github.com>
2022-09-21 22:10:33 +0530

    Create Hello Morse Code.cpp
    morse code

---
+++ Hello Morse Code.cpp
@@ -0,0 +1,52 @@
+#include <Arduino.h>
+
+void dot(bool last = false){
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(200);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(last ? 700 : 300);
+}
+void dash(bool last = false){
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(700);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(last ? 700 : 300);
+}
+
+void h(){
+  dot();
+  dot();
+  dot();
+  dot(true);
+}
+
+void e(){
+  dot(true);
+ 
+}
+
+void l(){
+  dot();
+  dot(true);
+}
+
+void o(){
+  dash();
+  dash();
+  dash(true);
+}
+
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+ 
+void loop() {
+  h();
+  e();
+  l();
+  l();
+  o();
+  delay(3000)
+}
